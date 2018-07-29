7f89397cd3ff953a20eb4ae0169ae13b01277aa0
minhazRTK <minhaz.rtk@gmail.com>
2018-07-29 20:36:56 +0600

    img,txt field and button actions added

--- app/src/main/java/com/batch2/m0nk3y/firstapplication/FirstActivity.java
+++ app/src/main/java/com/batch2/m0nk3y/firstapplication/FirstActivity.java
@@ -2,6 +2,10 @@ package com.batch2.m0nk3y.firstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        final EditText etImgName = findViewById(R.id.et_name);
+        Button btnAddNmae = findViewById(R.id.btn_add_name);
+        final TextView txtName = findViewById(R.id.txt_img_name);
+        btnAddNmae.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etImgName.getText().toString();
+                txtName.setText(name);
+            }
+        });
     }
 }
